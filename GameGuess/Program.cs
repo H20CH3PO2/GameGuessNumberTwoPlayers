2f2c3048f4dde6e478960e76a7b7232cb8d8d344
Носов Артём Иванович Б20-514 <artiom-nj@mail.ru>
2021-06-20 15:03:38 +0300

    Добавьте файлы проекта.

---
+++ GameGuess/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GameGuess
+{
+    // Заспавнить 
+    class Program
+    {
+        public static int Foo(String str)
+        {
+            return 5;
+        }
+        static void Main(string[] args)
+        {
+            int inputNumberfortuneTeller = -1;
+            int inputNumberGuesser = -2;
+            bool isFirst = false;
+            do {
+                isFirst = !isFirst;
+                if (isFirst)
+                {
+                    Console.WriteLine("Put your number for riddle first player!");
+                    inputNumberfortuneTeller = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Put your number for guess second player!");
+                    inputNumberGuesser = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Put your number for riddle second player!");
+                    inputNumberfortuneTeller = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Put your number for guess first player!");
+                    inputNumberGuesser = Convert.ToInt32(Console.ReadLine());
+                }
+                
+            } while (inputNumberfortuneTeller != inputNumberGuesser);
+            if (!isFirst)
+            {
+                Console.WriteLine("Yahu! First pleyer win!");
+                Console.WriteLine(inputNumberfortuneTeller);
+            }
+            else
+            {
+                Console.WriteLine("Yahu! Second pleyer win!");
+                Console.WriteLine(inputNumberGuesser);
+            }
+
+        }
+    }
+}
